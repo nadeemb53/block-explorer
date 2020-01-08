c90e6a8511e5d7bee16c010d3da9144f720ea264
Nadeem Bhati <nadeemb53@gmail.com>
2020-01-08 23:15:50 +0530

    api to retrieve transactions with from address added {not tested}

---
+++ router/transactionRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const Transactions = require('../model/transactions');
+const transactionRouter = express.Router();
+
+transactionRouter.route('/:from')
+.get((req,res,next) => {
+    Transactions.find({"from":req.params.from})
+    .then((transaction) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(transaction);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+module.exports = transactionRouter;
